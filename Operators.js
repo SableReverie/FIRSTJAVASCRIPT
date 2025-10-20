079fb637fd476b63bc92444624a910d8305f6eac
Prince <princecultura10@gmail.com>
2025-10-20 13:23:21 +0800

    Create Operators.js for arithmetic operations example
    Added a JavaScript file demonstrating various arithmetic operators and their precedence.

---
+++ Operators.js
@@ -0,0 +1,30 @@
+// ARITHMETIC OPERATORS EXAMPLE
+
+let num = 10;
+
+num = num + 5; // Addition
+num = num - 3; // Subtraction
+num = num * 2; // Multiplication
+num = num / 4; // Division
+num = num % 3; // Modulus
+num = num ** 2; // Exponentiation
+
+num += 4; // Addition assignment
+num -= 2; // Subtraction assignment
+num *= 3; // Multiplication assignment
+num /= 2; // Division assignment
+num %= 2; // Modulus assignment
+num **= 3; // Exponentiation assignment 
+
+num++; // Increment
+num--; // Decrement
+
+console.log(num);
+  Operator Presedence
+  1. Parentheses ()
+  2. Exponentiation
+  3. Multiplication, Division, Modulus
+  4. Addition, Subtraction
+  5. Assignment
+
+
