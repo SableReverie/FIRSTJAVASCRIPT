b44036f17822e83b620184d6ebcc76a533c054de
Prince <princecultura10@gmail.com>
2025-10-20 12:18:40 +0800

    Add introductory JavaScript variable examples

---
+++ Introduction.js
@@ -0,0 +1,33 @@
+// Variable Declaration === let x;
+// Variable Assignment === x = 5;
+// (``) is different from ('')/("");
+// let age = 25;
+// let price = 10.99;
+// let gpa = 2.1;  // ${age}, ${price}, ${gpa};
+
+//let firstName = "John";
+
+//console.log(typeof firstName); // string
+// let FirstName = "Prince";
+// let age = 18;
+// let email = "princegian@gmail.com";
+
+// console.log(typeof FirstName);
+// console.log(typeof age);
+// console.log(typeof email);
+// Output: string, number, string
+
+// console.log(`My name is ${FirstName}`);
+// console.log(`My age is ${age}`);
+// console.log(`My email is ${email}`);
+// Output: My name is Prince, My age is 18, My email is
+
+
+let fullName = "John Doe";
+let age = 25;
+let student = false;
+
+document.getElementById("p1").textContent = `Your name: ${fullName}`;
+document.getElementById("p2").textContent = `Your age: ${age}`;
+document.getElementById("p3").textContent = `Enrolled: ${student}`;
+*/
