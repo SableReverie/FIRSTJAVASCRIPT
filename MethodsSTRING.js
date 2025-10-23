5f3d249a483ddc36a417378dc6d951cd471c849a
Prince <princecultura10@gmail.com>
2025-10-23 15:12:08 +0800

    Implement method chaining for username formatting
    Refactor username processing to use method chaining for cleaner code.

---
+++ MethodsSTRING.js
@@ -0,0 +1,25 @@
+// METHOD CHAINING = Calling one method after another on the same object.
+
+// ------- NO METHOD CHAINING -------
+/*
+let username = window.prompt("Enter your username: ");
+
+username = username.trim();
+let letter = username.charAt(0);
+letter = letter.toUpperCase();
+
+let extraChars = username.slice(1);
+extraChars = extraChars.toLowerCase();
+username = letter + extraChars;
+
+console.log(username);
+*/
+
+// ------- METHOD CHAINING --------
+
+let username = window.prompt("Enter your username: ");
+
+username = username.trim().charAt(0).toUpperCase() + username.trim().slice(1).toLowerCase();
+console.log(username);
+
+
